fd53cc38e61f1a48419c8a21c6e3dcc0ac2d2d2f
ism47914261 <36157469+ism47914261@users.noreply.github.com>
2018-02-22 08:13:23 +0100

    Add files via upload

---
+++ ejercicio-mayor.py
@@ -0,0 +1,11 @@
+# coding = utf8
+
+
+numero = (input("Introduzca un numero:"))
+numero2 = (input("Introduzca un numero:"))
+
+if numero >= numero2:
+	print numero 
+	
+elif numero <= numero2:
+	print numero2
